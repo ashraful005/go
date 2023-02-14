60ff2aee76b39ec348e12295d00ea14f32d0d6ec
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-02-14 21:59:39 +0600

    user input between a range

---
+++ user.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+
+fmt.Println("Enter name and age: ")
+var name string
+var age int
+
+fmt.Scanf("%s %d",&name,&age)
+fmt.Printf("%s is %d years old\n",name,age)
+if age>=1 && age<=10{
+fmt.Println("Age between 1 and 10")
+}else{
+fmt.Println("Age not between 1 and 10")
+}
+}
