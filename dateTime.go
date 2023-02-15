faf1cc14f59b3faf26b45852df8dcb6057bb7ef4
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-02-15 23:40:09 +0600

    extracting individual parts of date

--- dateTime.go
+++ dateTime.go
@@ -8,4 +8,12 @@ import (
 func main(){
 currentTime := time.Now()
 fmt.Println("The time is", currentTime)
+
+
+fmt.Println("The year is",currentTime.Year())
+fmt.Println("The month is",currentTime.Month())
+fmt.Println("The day is",currentTime.Day())
+fmt.Println("The hour is",currentTime.Hour())
+fmt.Println("The minute is",currentTime.Minute())
+fmt.Println("The second is",currentTime.Second())
 }
