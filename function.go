7a01ae2743c9a49265bd9a74ad9bd3c8f6370371
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-02-15 21:43:14 +0600

    char to decimal

---
+++ function.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+//Decimal number to character
+fmt.Printf("%c\n", 68)
+
+
+//Hexadecimal number to character
+fmt.Printf("%c",0x49)
+
+//character to decimal
+fmt.Printf("%d", 'c')
+}
