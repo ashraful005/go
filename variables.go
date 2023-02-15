959f680ccbb29e0641715806765442ce6e4ae7b4
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-02-15 23:08:25 +0600

    variable swap

--- variables.go
+++ variables.go
@@ -12,5 +12,14 @@ fmt.Scanf("%f",&age)
 sum=sum+age
 }
 avg=sum/5.0
-fmt.Printf("The average age is %f years", avg)
+fmt.Printf("The average age is %.2f years\n", avg)
+
+
+//variable swap
+var a,b int
+a=5
+b=7
+a,b = b,a
+fmt.Printf("a: %d\n",a)
+fmt.Printf("b: %d\n",b)
 }
