b59ac040c1b9bd06d95320efb67e48c5a6e7d14d
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-02-17 10:42:56 +0600

    variable and datatypes

--- errorH.go
+++ errorH.go
@@ -16,6 +16,12 @@ var age int = 23
 //multiple variable assignment
 var city, state string = "Cumilla", "Bangladesh"
 
-fmt.Println(name, age, city, state)
+//char
+var grade rune = 'A'
+
+//float
+var result float32 =50.95
+
+fmt.Println(name, age, city, state, grade, result)
 
 }
