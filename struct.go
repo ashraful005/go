7d2a87ec362dbdcd485f44ee12536f5ffded4bb5
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-02-18 00:35:30 +0600

    struct declaration and initialization

--- struct.go
+++ struct.go
@@ -18,5 +18,12 @@ func main(){
 //struct declaration
 var b1 Book
 
+//struct initialization
+b1.Title = "An Introduction to Programming in Go"
+b1.Author = "Caleb Doxsey"
+b1.ISBN = "978-1478355823"
+b1.Price = 10.50
+b1.Pages = 165
+
 fmt.Println(b1)
 }
