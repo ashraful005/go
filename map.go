b2f13fda44ee355d9dbbc9c7c6f93be31fd5bf10
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-02-17 19:56:59 +0600

    default value of map

---
+++ map.go
@@ -0,0 +1,17 @@
+package main 
+
+
+import ("fmt"
+	
+)
+
+func main(){
+//unordered collection of key-value pairs
+//map declaration
+var x map[string]int
+
+//map initialization
+
+
+fmt.Println(x)
+}
