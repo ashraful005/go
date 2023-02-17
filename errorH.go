44f2a3b5940411868e1ad6b2b6d9bdd9fc14d2c8
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-02-17 10:19:43 +0600

    variable declaration and initialization

---
+++ errorH.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+//variable declaration
+var name string
+
+//initialization
+name = "Ashraful"
+
+
+//declaration and initialization
+var age int = 23
+
+var city, state string = "Cumilla", "Bangladesh"
+
+fmt.Println(name, age, city, state)
+
+}
