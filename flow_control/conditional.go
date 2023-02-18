17ef47c0ca050047ad3ed47e410077a943bc62b4
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-02-18 11:23:07 +0600

    if-else if statement

--- flow_control/conditional.go
+++ flow_control/conditional.go
@@ -7,10 +7,21 @@ fmt.Print("Enter your age: ")
 var age int
 fmt.Scanf("%d", &age)
 
-//if-else statement
-if age<10{
+//if-else if statement
+if age>0 && age<3{
+
+fmt.Println("Infant")
+
+}else if age>2 && age<13{
+
 fmt.Println("Children")
-}else{
+
+}else if age>12 && age<20{
+
 fmt.Println("Teenager")
+
+}else if age>19{
+
+fmt.Println("Adult")
 }
 }
