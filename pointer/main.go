73c80f80aed0e0044c658e90c71a043d12ab1041
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-02-18 14:51:56 +0600

    pointer declaration and default value

--- pointer/main.go
+++ pointer/main.go
@@ -6,4 +6,9 @@ func main(){
 var x int
 fmt.Println("x value is ", x)
 fmt.Println("x memory address is ", &x)
+
+//pointer declaration
+var y *int
+fmt.Println("y value is ", y)
+fmt.Println("y memory address is ", &y)
 }
