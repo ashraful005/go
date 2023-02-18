ac0097bb41e65fe491dd2f1020fb65872c20e838
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-02-18 11:53:38 +0600

    for-range loop

--- flow_control/loop.go
+++ flow_control/loop.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 func main(){
+/*
 //for loop
 for i:=1;i<=10;i++ {
 fmt.Println(i)
+}
+*/
+
+//range for loop
+students := []string{"Asgor", "Mainul", "Anonnya"}
+for i, val := range students {
+fmt.Printf("At position %d, the character %s is present\n",i,val)
 }
 
 }
