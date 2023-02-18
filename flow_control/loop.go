97002cbd82560ddf2821cd32f3f7960b0111e697
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-02-18 11:58:43 +0600

    while loop in Go

--- flow_control/loop.go
+++ flow_control/loop.go
@@ -10,10 +10,19 @@ fmt.Println(i)
 }
 */
 
+/*
 //range for loop
 students := []string{"Asgor", "Mainul", "Anonnya"}
 for i, val := range students {
 fmt.Printf("At position %d, the character %s is present\n",i,val)
+}
+*/
+
+//while loop
+i := 0
+for i<10 {
+fmt.Println(i, " Hello!")
+i++
 }
 
 }
