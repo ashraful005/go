d011222a5012cce62a70fcf981755902b67b5a6a
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-02-19 11:35:34 +0600

    Golang website with net/http package

--- network/net_http/first/main.go
+++ network/net_http/first/main.go
@@ -7,10 +7,20 @@ import (
 
 func main() {
 	http.HandleFunc("/", home)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/contact", contact)
 	http.ListenAndServe(":8888", nil)
 
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, `Welcome to my first Golang webpage`)
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `Welcome to my about page`)
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `Welcome to my contact page`)
 }
