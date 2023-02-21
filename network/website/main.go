59b6ffd5065fe243e76bff04b692806edea4d212
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-02-21 16:26:05 +0600

    form handling

--- network/website/main.go
+++ network/website/main.go
@@ -10,6 +10,7 @@ func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/features", features)
 	http.HandleFunc("/docs", docs)
+	http.HandleFunc("/request", request)
 	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("./assets"))))
 	http.ListenAndServe(":8888", nil)
 }
@@ -24,34 +25,45 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func features(w http.ResponseWriter, r *http.Request)  {
+func features(w http.ResponseWriter, r *http.Request) {
 	//w.Header().Set("Content-Type", "text/html; charset=UTF-8")
 	ptmp, err := template.ParseFiles("template/base.gohtml")
-	if err != nil{
+	if err != nil {
 		fmt.Println(err.Error())
 	}
 	//fmt.Fprintf(w, "Welcome")
 
 	ptmp, err = ptmp.ParseFiles("wpage/features.gohtml")
-	if err != nil{
+	if err != nil {
 		fmt.Println(err.Error())
 	}
 
 	ptmp.Execute(w, nil)
 }
 
-func docs(w http.ResponseWriter, r *http.Request)  {
+func docs(w http.ResponseWriter, r *http.Request) {
 	//w.Header().Set("Content-Type", "text/html; charset=UTF-8")
 	ptmp, err := template.ParseFiles("template/base.gohtml")
-	if err != nil{
+	if err != nil {
 		fmt.Println(err.Error())
 	}
 	//fmt.Fprintf(w, "Welcome")
 
 	ptmp, err = ptmp.ParseFiles("wpage/docs.gohtml")
-	if err != nil{
+	if err != nil {
 		fmt.Println(err.Error())
 	}
 
 	ptmp.Execute(w, nil)
+}
+
+func request(w http.ResponseWriter, r *http.Request) {
+	//r.ParseForm()
+	name := r.FormValue("name")
+	company := r.FormValue("company")
+	email := r.FormValue("email")
+
+	fmt.Println(name, company, email)
+
+	fmt.Fprintf(w, `received`)
 }
